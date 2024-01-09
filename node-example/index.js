ea21ab3bdc75b00d4bd7b5b66f9f484fd0ad3e0b
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2024-01-09 16:01:36 +0100

    test.http

--- node-example/index.js
+++ node-example/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello ACS Course!</h1>')
 })
 
+app.get('/greet/:name', (req, res) => {
+  // call google tranlsation service
+
+  res.send(`<h2>Hello ${req.params.name}!</h2>`)
+})
+
+app.get('translate/:text', (req, res) => {
+   // call google tranlsation service
+
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
